179558356c832403214aafff10b398a4cc3ef586
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-04-24 12:03:26 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/24/2026, 12:03:27 PM
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        int a = 1, b = 2;
+
+        for(int i=3; i<=n; i++){
+            int c = a + b; 
+            a = b; 
+            b = c;
+        }
+      return b;
+    }
+};
