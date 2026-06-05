bbaca600d0deefa1fcbcde166e6b117191a8604a
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-06-05 14:00:35 +0530

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.cpp
@@ -0,0 +1,12 @@
+// Last updated: 6/5/2026, 2:00:42 PM
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+};
