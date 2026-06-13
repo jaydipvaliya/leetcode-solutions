8852b8b09a98c28e7af925ae5de14cc6592b4e20
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-06-13 10:16:22 +0530

    Create 2058-ConcatenationOfArray

---
+++ 2058-ConcatenationOfArray/2058-ConcatenationOfArray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/13/2026, 10:16:24 AM
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n);
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
+    }
+};
