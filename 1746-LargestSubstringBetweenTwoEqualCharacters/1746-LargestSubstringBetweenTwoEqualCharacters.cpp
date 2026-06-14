a8f203472fbe2409af5a4b59777beee12e10b596
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-06-14 21:50:01 +0530

    Create 1746-LargestSubstringBetweenTwoEqualCharacters

---
+++ 1746-LargestSubstringBetweenTwoEqualCharacters/1746-LargestSubstringBetweenTwoEqualCharacters.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/14/2026, 9:50:05 PM
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int ans = -1;
+        for(int i = 0; i<s.size(); i++){
+            for(int j = i + 1; j < s.size(); j++){
+                if(s[i] == s[j]){
+                    ans = max(ans, j - i - 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
