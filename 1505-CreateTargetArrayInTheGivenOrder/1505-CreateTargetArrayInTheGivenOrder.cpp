450bcb0ab7f95101c5d0823498dec74125706d66
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-07-13 21:03:11 +0530

    Create 1505-CreateTargetArrayInTheGivenOrder

---
+++ 1505-CreateTargetArrayInTheGivenOrder/1505-CreateTargetArrayInTheGivenOrder.cpp
@@ -0,0 +1,11 @@
+// Last updated: 7/13/2026, 9:03:14 PM
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int>ans;
+        for(int i=0; i<nums.size(); i++){
+            ans.insert(ans.begin()+index[i],nums[i]);
+        }
+        return ans;
+    }
+};
