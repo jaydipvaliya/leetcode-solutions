e0f344a7dd46da22817d6edde9a5e428dc6c590f
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-07-13 21:04:11 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -0,0 +1,11 @@
+// Last updated: 7/13/2026, 9:04:14 PM
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (int j = 0, i = m; j<n; j++){
+            nums1[i] = nums2[j];
+            i++;
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
