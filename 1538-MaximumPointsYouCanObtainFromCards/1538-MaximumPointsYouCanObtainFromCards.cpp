574ffd49deac102ce9cc8c79803109443ec9ece3
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-07-28 10:51:26 +0530

    Create 1538-MaximumPointsYouCanObtainFromCards

---
+++ 1538-MaximumPointsYouCanObtainFromCards/1538-MaximumPointsYouCanObtainFromCards.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/28/2026, 10:51:38 AM
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int res = 0;
+        for(int i=0;i<k;i++) res+=cardPoints[i];
+        
+        int curr=res;
+        for(int i=k-1;i>=0;i--) {
+            curr-=cardPoints[i];
+            curr+=cardPoints[cardPoints.size()-k+i];
+			
+          
+			res = max(res, curr);
+        }
+        
+        return res;
+    }
+};
