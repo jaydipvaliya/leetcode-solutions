494f042e5da6b03ca49b39f54551956751d02d1c
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-08-03 13:15:16 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.cpp
@@ -0,0 +1,13 @@
+// Last updated: 8/3/2026, 1:15:32 PM
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n <= 0) return false;
+
+        while (n % 2 == 0) n /= 2;
+        while (n % 3 == 0) n /= 3;
+        while (n % 5 == 0) n /= 5;
+
+        return n == 1;
+    }
+};
