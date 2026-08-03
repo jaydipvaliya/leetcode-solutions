e8e8f6808298fe7cfbbd653594a3d720496f2813
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-08-03 13:15:04 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 8/3/2026, 1:15:20 PM
+class Solution {
+public:
+    bool canWinNim(int n) {
+
+        if (n % 4 != 0)
+            return true;
+        else
+            return false;
+    }
+};
