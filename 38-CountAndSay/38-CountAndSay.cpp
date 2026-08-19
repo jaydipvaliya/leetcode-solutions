108a98f629676e4e6c34908b1f093a15b786e011
jaydip valiya <jaydip.valiya.cg@gmail.com>
2026-08-19 13:24:41 +0530

    Create 38-CountAndSay

---
+++ 38-CountAndSay/38-CountAndSay.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/19/2026, 1:25:09 PM
+class Solution {
+public:
+    string countAndSay(int n) {
+        string res = "1";
+        for (int i = 1; i < n; i++) {
+            string temp = "";
+            int count = 1;
+            for (int j = 1; j < res.size(); j++) {
+                if (res[j] == res[j - 1]) {
+                    count++;
+                } else {
+                    temp += to_string(count) + res[j - 1];
+                    count = 1;
+                }
+            }
+            temp += to_string(count) + res.back();
+            res = temp;
+        }
+        return res;
+    }
+};
